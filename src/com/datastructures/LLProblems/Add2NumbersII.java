ef5abd0e5cdfcc12484b975cbe28a8c53dd8dc52
RahilModi <rmodi10@gmail.com>
2019-02-18 20:12:35 -0800

    added better solution for add numbers II

--- src/com/datastructures/LLProblems/Add2NumbersII.java
+++ src/com/datastructures/LLProblems/Add2NumbersII.java
@@ -8,6 +8,34 @@ You are given two non-empty linked lists representing two non-negative integers.
 
 public class Add2NumbersII {
 
+    public ListNode addTwoNumbersV1(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+
+        while(l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        };
+        while(l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int sum = 0;
+        ListNode list = new ListNode(0);
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) sum += s1.pop();
+            if (!s2.empty()) sum += s2.pop();
+            list.val = sum % 10;
+            ListNode head = new ListNode(sum / 10);
+            head.next = list;
+            list = head;
+            sum /= 10;
+        }
+
+        return list.val == 0 ? list.next : list;
+    }
+
     private class ListNode {
         int val;
         ListNode next;
